f821f6d3114970540ef80922f9aed1c37353e23d
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2022-03-24 07:31:21 -0300

    ajuste funçoes js

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-const api = {
-    key: "d341e5cb9626afc4800921e7ba6f6c8f",
-    base: "https://api.openweathermap.org/data/2.5/",
-    lang: "pt_br",
-    units: "metric"
-
+function getUserPosition() {
+    let url;
+    navigator.geolocation.getCurrentPosition((pos) => {
+        let lat = pos.coords.latitude;
+        let.long = pos.coords.longitude;
+        url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=d341e5cb9626afc4800921e7ba6f6c8f`;
+        fetchApi(url);
+    })
 }
 
-const city = document.querySelector('.city')
-const date = document.querySelector('.date');
-const container_img = document.querySelector('.constainer-img');
-const container_temp = document.querySelector('.constainer-temp');
-const temp_number = document.querySelector('.constainer-temp div');
-const temp_unit = document.querySelector('.constainer.span');
-const weather_t = document.querySelector('.weather');
-const search_input = document.querySelector('.form-control');
-const search_button = document.querySelector('btn');
-const low_high = document.querySelector('.low-high');
-
-search_button.addEventListener('click', function() {
-    searchResults(search_input.value)
-})
-
-search_input.addEventListener('Keypress', enter)
-
-function enter(event) {
-    key = event.keyCode
-    if (key === 13) {
-        searchResults(search.input.value)
-    }
+function fetchApi(url) {
+    let city = document.querySelector('city');
+    let temp = document.querySelector('span');
+    fetch(url)
+        .then((data) => {
+            return data.json();
+        })
+        .then((data) => {
+            let tempInCelsius = ((5 / 9) * (data.main.temp - 32)).toFixed(1);
+            city.innerText = `Hoje a temperatura em ${data.name} é: `;
+            temp.innerText = tempInCelsius;
+        })
+        .catch((err) => {
+            city.innerText = `Impossível acessar o OpenWeather.verifique sua conexão.`;
+            temp.innerText = `-`;
+        })
 }
+
+getUserPosition();
